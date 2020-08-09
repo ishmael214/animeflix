eb8a3def9553f92b73e2ed1845a5697fffbd9cb6
ishmael214 <64385652+ishmael214@users.noreply.github.com>
2020-08-09 04:21:48 -0500

    javascript added. tweak concept of project later

---
+++ app.js
@@ -0,0 +1,43 @@
+const slides = document.querySelectorAll('.slides');
+const dots = document.querySelectorAll('.column');
+let slideIndex = 0;
+showSlide();
+function showSlide(n) {
+   if(slideIndex > slides.length -1) {
+       slideIndex = slides.length -1;
+   }
+
+   if (slideIndex < 0) {
+       slideIndex = 0;
+   }
+    for(let i=0; i<slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (let i=0; i<dots.length; i++){
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].className += " active";
+}
+
+dots.forEach((item, index) => {
+    item.addEventListener('click', ()=> {
+        showSlide(slideIndex = index);
+    })
+});
+
+const nav = document.querySelector('.nav');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+next.addEventListener('click', () => {
+    nav.scrollLeft += dots[0].offsetWidth;
+    showSlide(slideIndex += 1);
+})
+
+prev.addEventListener('click', () => {
+    nav.scrollLeft -= dots[0].offsetWidth;
+    showSlide(slideIndex -= 1);
+})
